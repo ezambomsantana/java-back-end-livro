c04bc0f7ac7b7f57dd10eb13bd7ec63896d748cf
Eduardo Santana <ezambomsantana@gmail.com>
2022-09-26 10:02:59 -0300

    adiciona simple-user-api

---
+++ simple-user-api/src/main/java/com/santana/java/back/end/controller/UserController.java
@@ -0,0 +1,78 @@
+package com.santana.java.back.end.controller;
+
+import com.santana.java.back.end.dto.UserDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.PostConstruct;
+import javax.validation.Valid;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    public static List<UserDTO> usuarios = new ArrayList<UserDTO>();
+
+    @PostConstruct
+    public void initiateList() {
+        UserDTO userDTO = new UserDTO();
+        userDTO.setNome("Eduardo");
+        userDTO.setCpf("123");
+        userDTO.setEndereco("Rua a");
+        userDTO.setEmail("eduardo@email.com");
+        userDTO.setTelefone("1234-3454");
+        userDTO.setDataCadastro(LocalDateTime.now());
+
+        UserDTO userDTO2 = new UserDTO();
+        userDTO2.setNome("Luiz");
+        userDTO2.setCpf("456");
+        userDTO2.setEndereco("Rua b");
+        userDTO2.setEmail("luiz@email.com");
+        userDTO2.setTelefone("1234-3454");
+        userDTO2.setDataCadastro(LocalDateTime.now());
+
+        UserDTO userDTO3 = new UserDTO();
+        userDTO3.setNome("Bruna");
+        userDTO3.setCpf("678");
+        userDTO3.setEndereco("Rua c");
+        userDTO3.setEmail("bruna@email.com");
+        userDTO3.setTelefone("1234-3454");
+        userDTO3.setDataCadastro(LocalDateTime.now());
+
+        usuarios.add(userDTO);
+        usuarios.add(userDTO2);
+        usuarios.add(userDTO3);
+    }
+
+    @GetMapping
+    public List<UserDTO> getUsers() {
+        return usuarios;
+    }
+
+    @GetMapping("/{cpf}")
+    public UserDTO getUsersFiltro(@PathVariable String cpf) {
+        return usuarios
+                .stream()
+                .filter(userDTO -> userDTO.getCpf().equals(cpf))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("User not found."));
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public UserDTO inserir(@RequestBody @Valid UserDTO userDTO) {
+        userDTO.setDataCadastro(LocalDateTime.now());
+        usuarios.add(userDTO);
+        return userDTO;
+    }
+
+    @DeleteMapping("/{cpf}")
+    public boolean remover(@PathVariable String cpf) {
+        return usuarios
+                .removeIf(userDTO -> userDTO.getCpf().equals(cpf));
+    }
+
+}
---
+++ simple-user-api/src/main/java/com/santana/java/back/end/dto/UserDTO.java
@@ -0,0 +1,28 @@
+package com.santana.java.back.end.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO {
+
+    @NotBlank(message = "Nome é obrigatório")
+    private String nome;
+    @NotBlank(message = "CPF é obrigatório")
+    private String cpf;
+    private String endereco;
+    @NotBlank(message = "E-mail é obrigatório")
+    private String email;
+    private String telefone;
+    private LocalDateTime dataCadastro;
+
+}
