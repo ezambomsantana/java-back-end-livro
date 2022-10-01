8af7acf696b4b08994f324c1fb3bd0f2904f2924
Eduardo Santana <ezambomsantana@gmail.com>
2022-10-01 15:03:05 -0300

    update client

--- shopping-client/src/main/java/com/santana/java/back/end/dto/UserDTO.java
+++ shopping-client/src/main/java/com/santana/java/back/end/dto/UserDTO.java
@@ -1,7 +1,16 @@
 package com.santana.java.back.end.dto;
 
-import java.util.Date;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserDTO {
 
 	private String nome;
@@ -10,62 +19,6 @@ public class UserDTO {
 	private String key;
 	private String email;
     private String telefone;
-    private Date dataCadastro;
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public String getCpf() {
-		return cpf;
-	}
-
-	public void setCpf(String cpf) {
-		this.cpf = cpf;
-	}
-
-	public String getEndereco() {
-		return endereco;
-	}
-
-	public void setEndereco(String endereco) {
-		this.endereco = endereco;
-	}
-	
-	public String getKey() {
-		return key;
-	}
-
-	public void setKey(String key) {
-		this.key = key;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getTelefone() {
-		return telefone;
-	}
-
-	public void setTelefone(String telefone) {
-		this.telefone = telefone;
-	}
-
-	public Date getDataCadastro() {
-		return dataCadastro;
-	}
-
-	public void setDataCadastro(Date dataCadastro) {
-		this.dataCadastro = dataCadastro;
-	}
+    private LocalDateTime dataCadastro;
 	
 }
