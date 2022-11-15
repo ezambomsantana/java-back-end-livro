7c7e045971b12488df9e1ba123fc1f4c75bcd186
Eduardo Santana <ezambomsantana@gmail.com>
2022-11-15 17:57:53 -0300

    atualiza java

--- gateway/src/main/java/com/santana/java/back/end/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/santana/java/back/end/gateway/GatewayApplication.java
@@ -1,5 +1,6 @@
 package com.santana.java.back.end.gateway;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
@@ -13,15 +14,25 @@ public class GatewayApplication {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
 
+
+	@Value("${USER_API_URL:http://localhost:8080}")
+	private String userApiURL;
+
+	@Value("${PRODUCT_API_URL:http://localhost:8081}")
+	private String productApiURL;
+
+	@Value("${SHOPPING_API_URL:http://localhost:8082}")
+	private String shoppingApiURL;
+
 	@Bean
 	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
 		return builder.routes()
-				.route("path_route", r -> r.path("/user/**")
-						.uri("http://localhost:8080"))
-				.route("path_route", r -> r.path("/product/**")
-						.uri("http://localhost:8081"))
-				.route("path_route", r -> r.path("/shopping/**")
-						.uri("http://localhost:8082"))
+				.route("user_route", r -> r.path("/user/**")
+						.uri(userApiURL))
+				.route("product_route", r -> r.path("/product/**")
+						.uri(productApiURL))
+				.route("shopping_route", r -> r.path("/shopping/**")
+						.uri(shoppingApiURL))
 				.build();
 
 	}
